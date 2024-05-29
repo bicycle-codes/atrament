13f7256affd82db0ecf1323725188cfb8c711fbc
Nick Thomas <nichoth@nichoth.com>
2024-05-29 16:49:41 -0700

    update build so it doesnt minify

--- rollup.config.min.mjs
+++ rollup.config.min.mjs
@@ -1,20 +1,22 @@
+import terser from '@rollup/plugin-terser';
 import webWorkerLoader from 'rollup-plugin-web-worker-loader';
 
 export default {
   input: 'src/index.js',
   output: [
     {
-      file: 'dist/index.js',
+      file: 'dist/index.min.js',
+      plugins: [
+        terser(),
+      ],
     },
     {
-      file: 'dist/index.js',
+      file: 'dist/index.min.cjs',
       format: 'cjs',
       exports: 'named',
     },
   ],
   plugins: [
-    webWorkerLoader({
-      targetPlatform: 'browser',
-    }),
+    webWorkerLoader(),
   ],
 };
