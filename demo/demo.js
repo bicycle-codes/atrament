3cebbdd37fc851daca1f003d9ab457fdaed803cb
nichoth <nichoth@nichoth.com>
2024-10-03 15:41:32 -0700

    setup for debug

--- demo/demo.js
+++ demo/demo.js
@@ -4,10 +4,12 @@ import Atrament from '../dist/index.js';
 // first, we need to set up the canvas
 const canvas = document.getElementById('sketcher');
 
+const rect = canvas.getBoundingClientRect()
+
 // instantiate Atrament
 const atrament = new Atrament(canvas, {
-  width: canvas.offsetWidth,
-  height: canvas.offsetHeight,
+  width: canvas.offsetWidth + (rect.x),
+  height: canvas.offsetHeight + (rect.y),
 });
 
 console.log('atrament', atrament);
