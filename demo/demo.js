60da43de40aef60101ab2bfc8ed3a64f85ca1019
nichoth <nichoth@nichoth.com>
2024-10-03 17:33:05 -0700

    rm obsolete code

--- demo/demo.js
+++ demo/demo.js
@@ -4,21 +4,8 @@ import Atrament from '../dist/index.js';
 // first, we need to set up the canvas
 const canvas = document.getElementById('sketcher');
 
-const rect = canvas.getBoundingClientRect();
-
-window.rect = rect;
-window.canvas = canvas;
-const { x, y } = rect;
-
 // instantiate Atrament
 const atrament = new Atrament(canvas, {
-  // width: canvas.offsetWidth + 20,
-  // height: canvas.offsetHeight + 20,
-  // width: canvas.offsetWidth + x,
-  // height: canvas.offsetHeight + y,
   width: canvas.offsetWidth,
   height: canvas.offsetHeight,
 });
-
-console.log('canvas', canvas);
-console.log('atrament', atrament);
